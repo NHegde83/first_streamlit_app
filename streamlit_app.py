65860b9f674f77285a916275896f76310976e9b1
NHegde83 <141329736+NHegde83@users.noreply.github.com>
2023-08-07 10:30:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
-
+ 
 streamlit.header('Breakfast Menu : 🥣 🥗 🐔 🥑 🍞 ')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
