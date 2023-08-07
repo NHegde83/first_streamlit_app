3281a539da497ef3f6a0032f2b79160c7e4f5076
NHegde83 <141329736+NHegde83@users.noreply.github.com>
2023-08-07 11:37:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,9 @@ my_cur = my_cnx.cursor()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
+
 streamlit.write('Thanks for Adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
